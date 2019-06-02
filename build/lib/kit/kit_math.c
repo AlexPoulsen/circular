55ca75cfdcbc2f0d313d789a8f5af4a64806ff37
Alex Poulsen <AlexPoulsen@users.noreply.github.com>
2019-06-02 19:18:38 -0400

    remove build folder

--- build/lib/kit/kit_math.c
+++
@@ -1,3 +0,0 @@
-#include "main.h"
-
-double kit_math__PI = 3.14159265358979323846264338327950288;
--- build/lib/kit/kit_mem.c
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-
-void * kit_mem__callocWrapper(size_t n)
-{
-    return calloc(n, (size_t) 1);
-}
-
-struct kit_mem_Allocator__box kit_mem__mallocator = { .__this = &(struct kit_mem__SimpleAllocator) {.alloc = malloc, .calloc = kit_mem__callocWrapper, .free = free}, .__vtable = &kit_mem_Allocator__6381ed4a392eb3039656f126 };
